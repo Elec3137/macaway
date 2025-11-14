9cee4e2e0f991d76f2b57fb8469b84f42c317090
Elec3137 <elec3137@proton.me>
2025-11-14 12:28:16 -0800

    style improvements (`stuff` is not a good var name)

--- src/main.rs
+++ src/main.rs
@@ -47,47 +47,50 @@ fn get_next_mouseclick_cords() -> Result<(u16, u16), Box<dyn Error>> {
 fn unbind_all() {
     mki::remove_any_button_bind();
     mki::remove_any_key_bind();
-    mki::remove_key_bind(mki::Keyboard::F1);
+    mki::remove_key_bind(Keyboard::F1);
 }
 
 fn record_macro() -> Result<Vec<(Option<Keyboard>, Option<(Mouse, u16, u16)>)>, Box<dyn Error>> {
-    let stuff = Arc::new(Mutex::new(Vec::<(
+    let macro_vec_mutex = Arc::new(Mutex::new(Vec::<(
         Option<Keyboard>,
         Option<(Mouse, u16, u16)>,
     )>::new()));
-    let stuff_clone = stuff.clone();
-    let stuff_clone1 = stuff.clone();
 
     let ignore_esc = Arc::new(AtomicBool::new(false));
-    let ignore_esc_clone = ignore_esc.clone();
+    let ignore_esc_ref = ignore_esc.clone();
 
     let (sender, receiver) = channel();
 
+    let macro_vec_ref1 = macro_vec_mutex.clone();
     mki::bind_any_button(Action::sequencing_mouse(
         move |button| match get_next_mouseclick_cords() {
             Ok((x, y)) => {
-                stuff.lock().unwrap().push((None, Some((button, x, y))));
+                macro_vec_ref1
+                    .lock()
+                    .unwrap()
+                    .push((None, Some((button, x, y))));
                 println!("Mouse button pressed {:?} at {},{}", button, x, y);
                 ignore_esc.store(true, Ordering::SeqCst);
             }
             Err(e) => eprintln!("Ignoring mouse click: {}", e),
         },
     ));
+    let macro_vec_ref2 = macro_vec_mutex.clone();
     mki::bind_any_key(Action::sequencing_kb(move |key| {
-        if key == Keyboard::Escape && ignore_esc_clone.load(Ordering::SeqCst) {
+        if key == Keyboard::Escape && ignore_esc_ref.load(Ordering::SeqCst) {
             eprintln!("Ignoring Escape key (slurp cancel keybind)");
-            ignore_esc_clone.store(false, Ordering::SeqCst);
+            ignore_esc_ref.store(false, Ordering::SeqCst);
         } else if key == Keyboard::F1 {
             unbind_all();
             sender.send(0).unwrap();
         } else {
-            stuff_clone.lock().unwrap().push((Some(key), None));
+            macro_vec_ref2.lock().unwrap().push((Some(key), None));
             println!("Keyboard key pressed {:?}", key);
         }
     }));
 
     receiver.recv()?;
-    Ok(stuff_clone1.lock().unwrap().to_vec())
+    Ok(macro_vec_mutex.lock().unwrap().to_vec())
 }
 
 fn play_macro(
